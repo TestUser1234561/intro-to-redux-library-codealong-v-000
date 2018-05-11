86f51123a294a13ee66b77312eee7017d0fa885e
Morgan Fudge <mfudge92@gmail.com>
2018-05-11 19:39:43 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from 'src/reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
- 
+
 const store = createStore(shoppingListItemReducer); /* code change */
- 
+
 ReactDOM.render(
-  <App store={store} />, /* code change */ 
+  <App store={store} />, /* code change */
   document.getElementById('root')
 );
